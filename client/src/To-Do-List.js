6daa03d464c40c856915f1b087a86fb5cf8fdb4a
Matheus Azzi <99417994+azzidev@users.noreply.github.com>
2025-02-09 13:19:40 -0300

    install react and fix dependencies

---
+++ client/src/To-Do-List.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+import axios from "axios";
+import {Card, Header, Form, Input, Icon} from "semant-ui-react"
+
+let endpoint = "http://localhost:9000";
+
+class ToDoList extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            task: "",
+            items: [],
+        };
+    }
+    ComponentDidMount(){
+        this.getTask();
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="row">
+                    <Header className="header" as="h2" color="lightgreen">TO DO LIST</Header>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ToDoList;
